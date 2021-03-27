8935d6ae7a0557b391fdb29b571b2c06e933b43a
UnweptToast <dsanchith@gmail.com>
2021-03-27 14:48:10 +0530

    first commit

--- App.js
+++ App.js
@@ -1,21 +1,133 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import {
+  View,
+  Text,
+  StatusBar,
+  StyleSheet,
+  TouchableOpacity,
+  TextInput,
+  ScrollView,
+  Keyboard,
+  TouchableWithoutFeedback,
+} from "react-native";
+
+import Header from "./components/Header";
+
+import dict from "./database";
 
 export default function App() {
+  const [text, setText] = useState("");
+
+  const [displayText, setDisplayText] = useState("");
+
+  const [definition, setDefinition] = useState("");
+
+  const [type, setType] = useState("");
+
+  const getWord = async (word) => {
+    var search = word.toLowerCase();
+    if (dict[search]) {
+      //console.log("Exists");
+      setDefinition(dict[search].definition);
+      setType(dict[search].lexicalCategory);
+    } else {
+      setDefinition("Not Found");
+      setType("Not Found");
+    }
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{ flex: 1, backgroundColor: "#000" }}>
+      <StatusBar barStyle="light-content" />
+      <Header />
+      <View style={styles.inputContainer}>
+        <View>
+          <TextInput
+            placeholder="Search words and definitions.."
+            style={styles.input}
+            placeholderTextColor="grey"
+            onChangeText={(val) => {
+              setText(val);
+            }}
+          />
+        </View>
+      </View>
+      <TouchableOpacity
+        style={styles.button}
+        onPress={() => {
+          setDisplayText(text);
+          getWord(text);
+          Keyboard.dismiss();
+        }}
+      >
+        <Text style={{ color: "whitesmoke", fontSize: 30 }}>Go</Text>
+      </TouchableOpacity>
+      <ScrollView
+        style={{
+          backgroundColor: "#111",
+          width: "85%",
+          alignSelf: "center",
+          marginTop: 40,
+          borderRadius: 30,
+          marginBottom: 40,
+        }}
+      >
+        <View style={styles.container}>
+          <Text style={styles.word}>Word: {displayText}</Text>
+          <Text style={styles.definition}>Definition: {definition}</Text>
+          <Text style={styles.type}>Word Type: {type}</Text>
+        </View>
+      </ScrollView>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  inputContainer: {
+    height: "20%",
+    width: "100%",
+    alignItems: "center",
+    justifyContent: "flex-end",
+  },
+  button: {
+    alignSelf: "center",
+    alignItems: "center",
+    backgroundColor: "orange",
+    paddingVertical: 8,
+    paddingHorizontal: 20,
+    borderRadius: 22,
+    marginTop: 15,
+  },
+  input: {
+    color: "white",
+    backgroundColor: "#151515",
+    fontSize: 20,
+    paddingVertical: 20,
+    width: "80%",
+    borderRadius: 25,
+    paddingHorizontal: 20,
+    marginBottom: 30,
+  },
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    marginTop: 15,
+  },
+  word: {
+    color: "white",
+    fontSize: 30,
+    margin: 30,
+  },
+  definition: {
+    color: "white",
+    fontSize: 30,
+    margin: 30,
+    textAlign: "center",
+  },
+  type: {
+    color: "white",
+    fontSize: 30,
+    margin: 30,
   },
 });
+
+//https://rupinwhitehatjr.github.io/dictionary/%22+searchKeyword+%22.json
---
+++ components/Header.js
@@ -0,0 +1,42 @@
+import React from "react";
+import {
+  Text,
+  View,
+  StyleSheet,
+  StatusBar,
+  Dimensions,
+  SafeAreaView,
+} from "react-native";
+
+function header() {
+  return (
+    <SafeAreaView style={styles.heading}>
+      <Text style={styles.title}>Pocket Dictionary</Text>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  headingView: {
+    alignItems: "center",
+    backgroundColor: "black",
+    height: Dimensions.get("screen").height * 0.17,
+  },
+  title: {
+    fontSize: 30,
+    color: "white",
+    fontWeight: "bold",
+  },
+  heading: {
+    justifyContent: "center",
+    height: Dimensions.get("screen").height * 0.17,
+    alignItems: "center",
+    backgroundColor: "#111",
+    borderRadius: Dimensions.get("screen").width * 0.1,
+    width: "90%",
+    marginTop: "12%",
+    alignSelf: "center",
+  },
+});
+
+export default header;
